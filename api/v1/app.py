e976db4d010c52d17afb1e20ccc0ade03876ba72
Amr Adel <amr96.adel@gmail.com>
2024-02-24 14:13:36 +0200

    TASK 05

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-
+from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -20,5 +20,8 @@ def not_found(error):
     return {"error": "Not found"}, 404
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+
+    app.run(host=host, port=port, threaded=True)
